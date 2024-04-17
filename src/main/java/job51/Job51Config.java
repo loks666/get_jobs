3713fc04c5f37ad43e1f5b904c43cf8a9858be60
jax <284190056@qq.com>
2024-04-17 17:06:43 +0800

    🧔🏻‍fix some 51job bugs and boss config.yaml

---
+++ src/main/java/job51/Job51Config.java
@@ -0,0 +1,40 @@
+package job51;
+
+import lombok.Data;
+import lombok.SneakyThrows;
+import utils.JobUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class Job51Config {
+
+
+    /**
+     * 搜索关键词列表
+     */
+    private List<String> keywords;
+
+    /**
+     * 城市编码
+     */
+    private List<String> jobArea;
+
+    /**
+     * 薪资范围
+     */
+    private List<String> salary;
+
+
+    @SneakyThrows
+    public static Job51Config init() {
+        Job51Config config = JobUtils.getConfig(Job51Config.class);
+        // 转换城市编码
+        config.setJobArea(config.getJobArea().stream().map(value -> Job51Enum.jobArea.forValue(value).getCode()).collect(Collectors.toList()));
+        // 转换薪资范围
+        config.setSalary(config.getSalary().stream().map(value -> Job51Enum.Salary.forValue(value).getCode()).collect(Collectors.toList()));
+        return config;
+    }
+
+}
