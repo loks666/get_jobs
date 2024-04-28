df0daed161d605e60ed78c9888a988c0d700caf0
jax <284190056@qq.com>
2024-04-28 14:00:12 +0800

    🌼update wait time.

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -8,6 +8,6 @@ public class Constant {
     public static ChromeDriver CHROME_DRIVER;
     public static Actions ACTIONS;
     public static WebDriverWait WAIT;
-    public static int WAIT_TIME = 30;
+    public static int WAIT_TIME = 60;
     public static String UNLIMITED_CODE = "0";
 }
