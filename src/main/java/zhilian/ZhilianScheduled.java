dc800ee9d13b986c20239ce0a1fc2090eff2fcb2
jax <284190056@qq.com>
2024-09-16 01:04:58 +0800

    🏝️modify timed delivery.

--- src/main/java/zhilian/ZhilianScheduled.java
+++ src/main/java/zhilian/ZhilianScheduled.java
@@ -1,38 +1,18 @@
 package zhilian;
 
 import lombok.extern.slf4j.Slf4j;
-import utils.Bot;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import static utils.JobUtils.formatDuration;
-import static utils.JobUtils.getDelayTime;
+import utils.JobUtils;
+import utils.Platform;
 
 @Slf4j
 public class ZhilianScheduled {
 
     public static void main(String[] args) {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
-        postJobs();
-        scheduleTask(scheduler, ZhilianScheduled::postJobs);
+        JobUtils.runScheduled(Platform.ZHILIAN);
     }
 
-    private static void postJobs() {
+    public static void postJobs() {
         safeRun(() -> ZhiLian.main(null));
-    }
-
-    private static void scheduleTask(ScheduledExecutorService scheduler, Runnable task) {
-        long delay = getInitialDelay();
-        String msg = "【智联招聘】距离下次投递还有：" + formatDuration(delay);
-        log.info(msg);
-        Bot.sendMessage(msg);
-        scheduler.scheduleAtFixedRate(task, delay, TimeUnit.DAYS.toSeconds(1), TimeUnit.SECONDS);
-    }
-
-    private static long getInitialDelay() {
-        return getDelayTime();
     }
 
     private static void safeRun(Runnable task) {
