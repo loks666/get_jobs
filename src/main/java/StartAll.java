937de5ef899d6bcc8eb9b7499a37fdd4c15b5067
loks666 <284190056@qq.com>
2024-12-21 02:21:33 +0800

    👨🏻‍🔧add StartAll.java.

---
+++ src/main/java/StartAll.java
@@ -0,0 +1,12 @@
+import boss.Boss;
+import job51.Job51;
+import liepin.Liepin;
+
+public class StartAll {
+
+    public static void main(String[] args) {
+        Boss.main(null);
+        Liepin.main(null);
+        Job51.main(null);
+    }
+}
